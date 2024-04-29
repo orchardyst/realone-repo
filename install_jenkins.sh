6ef96a48d6d24c368ee81811120304fea720e1af
orchardyst <115893088+orchardyst@users.noreply.github.com>
2024-04-29 03:39:20 +0300

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo apt update
 sudo apt install openjdk-11-jdk -y
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5BA31D57EF5975CA
@@ -31,6 +32,7 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo usermod -aG docker jenkins
 sudo usermod -aG docker ubuntu
 sudo usermod -aG docker $USER
+sudo chmod 666 /var/run/docker.sock
 curl -LO https://dl.k8s.io/release/v1.21.0/bin/linux/amd64/kubectl
 curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
@@ -40,8 +42,8 @@ mv ./kubectl ~/.local/bin/kubectl
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl
 sudo apt-get install -y apt-transport-https
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.21/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.21/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update -y
 sudo apt-get install -y kubectl=1.21.0-00
 sudo apt install awscli -y
